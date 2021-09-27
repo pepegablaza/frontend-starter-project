d27b4788289fe8eead74d0dc0b4291c2c19d8c2e
pepegablaza <marioblazevic28@gmail.com>
2021-09-27 22:47:02 +0200

    commit 3

--- src/assets/scripts/constants.js
+++ src/assets/scripts/constants.js
@@ -3,6 +3,6 @@ const cartContent = document.querySelector(".dropdown-content");
 const btnUp = document.querySelector(".btn-up");
 const btnDown = document.querySelector(".btn-down");
 const list = document.querySelector(".dropdown-content-list");
-const title = document.querySelector(".title");
+const title = document.querySelector(".dropdown-content-title");
 
 export { cartButton, cartContent, btnUp, btnDown, list, title };
